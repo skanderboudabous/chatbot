cc9416ad922086cb8b507167d76459c940fb5720
medmsd <mseddi.mohamed@iit.ens.tn>
2020-05-18 00:54:50 +0100

    fixed pre-register multi language link

--- lib/iit-view.dart
+++ lib/iit-view.dart
@@ -1,11 +1,11 @@
-import 'package:ChatBot/utils/const.dart';
 import 'package:easy_web_view/easy_web_view.dart';
 import 'package:flutter/material.dart';
+import 'package:internationalization/internationalization.dart';
 class IITView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return EasyWebView(
-      src: iitUrl,
+      src: Strings.of(context).valueOf("Ins"),
     );
   }
 }
--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,3 +1,2 @@
 const String TITLE = "IIT Bot";
 const String jsonPath="assets/credentials.json";
-const String iitUrl="https://iit.tn/en/pre-inscription/";
